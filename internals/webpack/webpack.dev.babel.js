69500534b08072f71edd796190a6c1ace6ea209c
Amin Mousavi <samousavih@gmail.com>
2017-10-15 20:14:42 +1000

    use inline source map

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -52,9 +52,9 @@ module.exports = require('./webpack.base.babel')({
   },
 
   // Emit a source map for easier debugging
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'inline-source-map',
 
-  performance: {
+  performance: {  
     hints: false,
   },
 });
