b56a33fa6c50bd3128d0609627dbd7b88db478e3
Amin Mousavi <samousavih@gmail.com>
2017-11-12 17:25:24 +1000

    added navigation using react route and redux

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,40 +1,67 @@
 import React, { PropTypes } from 'react';
 import { Layout, Menu } from 'antd';
+import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import 'antd/dist/antd.css';
+import { makeSelectLocationState } from './selectors';
 const { Header, Content, Footer } = Layout;
 
-export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {
     children: PropTypes.node,
+    onNavigate: PropTypes.func,
   };
 
   render() {
+    const { onNavigate } = this.props;
     return (
       <Layout>
-        <Header style={{ background: '#fff', padding: 0 }}>
+        <Header style={{ background: '#fff', padding: 0, height: 32 }}>
           <div className="logo" />
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['1']}
+            defaultSelectedKeys={['home']}
             style={{ lineHeight: '32px' }}
+            onClick={onNavigate}
           >
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Gallery</Menu.Item>
-            <Menu.Item key="3">About us</Menu.Item>
-            <Menu.Item key="4">Contact us</Menu.Item>
+            <Menu.Item key="home">Home</Menu.Item>
+            <Menu.Item key="gallery">Gallery</Menu.Item>
+            <Menu.Item key="aboutus">About us</Menu.Item>
+            <Menu.Item key="contactus">Contact us</Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+        <Content style={{ margin: '10px 16px 0' }}>
+          <div style={{ padding: 20, background: '#fff', minHeight: 360 }}>
             {React.Children.toArray(this.props.children)}
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2016 Created by Ant UED
+          Tooranj Bano ©2017
         </Footer>
       </Layout>
     );
   }
 }
+
+
+const mapStateToProps = createStructuredSelector({
+  location: makeSelectLocationState(),
+});
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onNavigate: (e) => {
+      if (e.key === 'gallery') {
+        dispatch(push('/Gallery'));
+      } else if (e.key === 'home') {
+        dispatch(push('/'));
+      }
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
